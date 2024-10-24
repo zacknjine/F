0650003869343f7fe783c481683e07b1d218c7f7
zack.njine <zackwachira45@gmail.com>
2024-10-24 21:59:48 +0300

    start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {Config} */ 
+/** @typedef {import('tailwindcss').Config} Config */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
